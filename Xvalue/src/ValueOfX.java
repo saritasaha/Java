10ff91f91d7df79dc41eaee4ed1318eae7a70ecd
saritasaha <saritasaha2009@gmail.com>
2018-04-13 00:30:54 +0530

    what's wrong in it?

---
+++ Xvalue/src/ValueOfX.java
@@ -0,0 +1,17 @@
+
+public class ValueOfX {
+	
+	public static void main(String[] args) {
+		int x=1;
+		System.out.println("Before the Loop x is" + x);
+		while (x > 12)
+			x = x - 1;{
+		System.out.println("inside");
+			}
+		for (int x1 = 0; x1 < 10; x1 = x1 + 1) {
+			System.out.println("x is now" + x);
+		
+	}
+		
+	}
+}
