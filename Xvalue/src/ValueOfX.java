543bdde01069f0fb04c839597e54c14182b097e7
saritasaha <saritasaha2009@gmail.com>
2018-04-14 02:36:24 +0530

    modified

--- Xvalue/src/ValueOfX.java
+++ Xvalue/src/ValueOfX.java
@@ -4,14 +4,14 @@ public class ValueOfX {
 	public static void main(String[] args) {
 		int x=1;
 		System.out.println("Before the Loop x is" + x);
-		while (x > 12)
-			x = x - 1;{
+		while (x > 12) {
+			x = x - 1;
 		System.out.println("inside the loop");
 			}
-		for (int x1 = 0; x1 < 10; x1 = x1 + 1) {
+		for (x=0; x < 10; x = x + 1) {
 			System.out.println("x is now" + x);
 		
-	}
+		}
 		
 	}
 }
